3b41f8cd982d427b38679fa6f771e3ecb5ab420d
Edouard Lopez <dev+git@edouard-lopez.com>
2015-05-24 00:36:40 +0200

    refactor(model): move LexicalUnit, Dictionary and Learner to parlr.py

--- parlr.py
+++ parlr.py
@@ -4,3 +4,38 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run()
+
+
+class LexicalUnit(object):
+    def __init__(self, name):
+        self.definitions = []
+        self.name = name
+
+    def add_definition(self, definition):
+        self.definitions.append(definition)
+
+    def get_definitions(self, word):
+        for definition in self.definitions:
+            if word in definition:
+                return [definition]
+
+
+class Dictionary(object):
+    def __init__(self):
+        self.lexical_units = []
+
+    def add_lexical_unit(self, lexical_unit):
+        self.lexical_units.append(lexical_unit)
+
+
+class Learner(object):
+    def __init__(self):
+        self.level = 0
+        self.known_characters = []
+
+    def learn(self, lexical_unit):
+        self.known_characters.append(lexical_unit)
+
+    def forget(self, lexical_unit):
+        lexical_unit_index = self.known_characters.index(lexical_unit)
+        del self.known_characters[lexical_unit_index]
--- tests/test_parlr.py
+++ tests/test_parlr.py
@@ -1,18 +1,6 @@
 import unittest
 
-
-class LexicalUnit(object):
-    def __init__(self, name):
-        self.definitions = []
-        self.name = name
-
-    def add_definition(self, definition):
-        self.definitions.append(definition)
-
-    def get_definitions(self, word):
-        for definition in self.definitions:
-            if word in definition:
-                return [definition]
+from parlr import LexicalUnit, Dictionary, Learner
 
 
 class LexicalUnitTestCase(unittest.TestCase):
@@ -30,14 +18,6 @@ class LexicalUnitTestCase(unittest.TestCase):
         self.assertEqual(2, len(lexical_unit.definitions))
 
 
-class Dictionary(object):
-    def __init__(self):
-        self.lexical_units = []
-
-    def add_lexical_unit(self, lexical_unit):
-        self.lexical_units.append(lexical_unit)
-
-
 class DictionaryTestCase(unittest.TestCase):
     def test_dictionary_has_lexical_units(self):
         dictionary = Dictionary()
@@ -51,18 +31,6 @@ class DictionaryTestCase(unittest.TestCase):
 
         self.assertEqual([lexical_unit], dictionary.lexical_units)
 
-
-class Learner(object):
-    def __init__(self):
-        self.level = 0
-        self.known_characters = []
-
-    def learn(self, lexical_unit):
-        self.known_characters.append(lexical_unit)
-
-    def forget(self, lexical_unit):
-        lexical_unit_index = self.known_characters.index(lexical_unit)
-        del self.known_characters[lexical_unit_index]
 
 class LearnerTestCase(unittest.TestCase):
     def test_learner_knows_a_list_of_characters(self):
